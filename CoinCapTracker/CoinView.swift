f22844cefed9e2f8c9585f3d1e333b7c55547afb
netlanc <netlanc@yandex.ru>
2024-05-02 15:55:39 +0300

    Раскоментировал пару строк

--- CoinCapTracker/CoinView.swift
+++ CoinCapTracker/CoinView.swift
@@ -8,9 +8,9 @@ import SwiftUI
 import CoreData
 
 struct CoinView: View {
-
     
-//    @Binding var titleOn: Bool // из задачи 2
+    
+    //    @Binding var titleOn: Bool // из задачи 2
     @AppStorage("titleOn") var titleOn = true
     
     @State private var isLoading = false
@@ -23,75 +23,29 @@ struct CoinView: View {
     var body: some View {
         
         NavigationView {
-            //if !coins.isEmpty {
-            List {
-                ForEach(coins, id: \.self) { coin in
-                    NavigationLink(destination: CoinDetails(coin: coin)) {
-                        CoinRow(coin: coin)
-//                        Text("\(coin.currency)")
+            if !coins.isEmpty {
+                List {
+                    ForEach(coins, id: \.self) { coin in
+                        NavigationLink(destination: CoinDetails(coin: coin)) {
+                            CoinRow(coin: coin)
+                            //                        Text("\(coin.currency)")
+                        }
                     }
                 }
+                .navigationTitle(titleOn ? "Монеты":"")
+            } else {
+                Text("Монеты пока не добавлены, перейдите в натройки что бы добавить монтеы")
+                    .multilineTextAlignment(.center)
+                
             }
-//            .toolbar {
-//                ToolbarItem {
-//                    Button(action: fetchData) {
-//                        Image(systemName: "arrow.clockwise")
-//                            .rotationEffect(.degrees(isLoading ? 360 : 0))
-//                            .animation(isLoading ? .linear(duration: 1).repeatForever(autoreverses: false) : .default)
-//                    }
-//                    .disabled(isLoading)
-//                }
-//                ToolbarItem {
-//                    Button(action: deleteAllCoins) {
-//                        Label("Delete All", systemImage: "trash")
-//                    }
-//                }
-//            }
-            .navigationTitle(titleOn ? "Монеты":"")
-//            } else {
-//                    Text("Монеты пока не добавлены, перейдите в натройки что бы добавить монтеы")
-//                        .multilineTextAlignment(.center)
-//                
-//            }
         }
         .navigationViewStyle(StackNavigationViewStyle())
         .alert(isPresented: $showAlert) {
             Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
         }
     }
-    /*
-    private func deleteAllCoins() {
-        DatabaseService.shared.deleteAllCoins()
-    }
     
-    private func fetchData() {
-        
-        withAnimation {
-            var urlString = "?action=items&id=";
-            let ids = coins.map { String($0.id) }
-            urlString += "[" + ids.joined(separator: ",") + "]"
-            
-            
-            print("urlString", urlString)
-            
-            
-            isLoading = true
-            NetworkService.shared.loadCoinData(urlString) { [self] coinDataArray, error in
-                isLoading = false
-                
-                if let error = error {
-                    alertMessage = error.localizedDescription
-                    showAlert = true
-                } else if let coinDataArray = coinDataArray {
-                    for coinData in coinDataArray {
-                        DatabaseService.shared.addCoin(data: coinData)
-                    }
-                    dataAdded.toggle()
-                }
-            }
-        }
-    }
-    */
+    
 }
 
 
